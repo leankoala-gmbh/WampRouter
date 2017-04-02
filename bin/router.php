54f92067e13e4559b0728c8b9c47a03f0f78852e
phmLabs <nils.langner@phmlabs.com>
2017-04-02 12:30:18 +0200

    readme

--- bin/router.php
+++ bin/router.php
@@ -4,11 +4,12 @@ if (!$loader = include __DIR__ . '/../vendor/autoload.php') {
     die('You must set up the project dependencies.');
 }
 
-$app = new \Cilex\Application('phmLabs WAMP Router v1.0');
+$app = new \Cilex\Application("\n<comment>phmLabs/WampRouter v1.0</comment> written by Nils Langner, based on voryx/thruway.");
 
 $app->command(new \phmLabs\WampRouter\Command\StartCommand());
 
 $app->command('foo', function ($input, $output) {
     $output->writeln('Example output');
 });
+
 $app->run();
--- readme.md
+++ readme.md
@@ -4,5 +4,17 @@ This router is based on voryx/thruway and is implementing the "Web Application M
 
 ## Usage
 ```
-php bin/server.php
+// run on 127.0.0.1:9090
+php bin/router.php start 
+
+// run on 127.0.0.1:9090 with more information
+php bin/router.php start -v
+
+// run on 127.0.0.1:8090
+php bin/router.php start -H 127.0.0.1 -p 8090
 ```
+
+## Credits
+This router is based on voryx/Thruway a great library to handle all kind of websocket stuff (https://github.com/voryx/Thruway).
+
+
